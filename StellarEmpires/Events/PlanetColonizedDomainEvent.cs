c70335fc78a2d961a63559f728a5c3dd27fe7127
Sébastien Eveno <sebastien.louis.eveno@gmail.com>
2024-11-07 21:50:42 +0100

    Add Planet and PlanetColonizedDomainEvent
    Fix

---
+++ StellarEmpires/Events/PlanetColonizedDomainEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace StellarEmpires.Events;
+
+public sealed record PlanetColonizedDomainEvent : IDomainEvent
+{
+	public Guid EntityId { get; init; }
+	public Guid Id { get; init; }
+	public DateTime OccurredOn { get; init; }
+	public Guid PlayerId { get; init; }
+	public DateTime ColonizedAt { get; init; }
+
+	public PlanetColonizedDomainEvent(Guid planetId, Guid playerId, DateTime colonizedAt)
+	{
+		Id = Guid.NewGuid();
+		OccurredOn = DateTime.UtcNow;
+		EntityId = planetId;
+		PlayerId = playerId;
+		ColonizedAt = colonizedAt;
+	}
+}
