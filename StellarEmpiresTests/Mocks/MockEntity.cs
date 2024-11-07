4f22ebeafc08809b927132ae5198b53faf46bf48
Sébastien Eveno <72524233+SebastienEveno@users.noreply.github.com>
2024-11-07 21:37:36 +0100

    Add entity abstract class (#5)
    Add MediatR

---
+++ StellarEmpiresTests/Mocks/MockEntity.cs
@@ -0,0 +1,28 @@
+﻿using StellarEmpires.Domain;
+using StellarEmpires.Events;
+
+namespace StellarEmpires.Tests.Mocks;
+
+public class MockEntity : Entity
+{
+	public DateTime LastEventOccurred { get; private set; }
+
+	public MockEntity() : base() { }
+
+	public MockEntity(Guid id) : base(id) { }
+
+	public void AddDomainEvent()
+	{
+		var domainEvent = new MockDomainEvent();
+
+		AddDomainEvent(domainEvent);
+	}
+
+	public override void Apply(IDomainEvent domainEvent)
+	{
+		if (domainEvent is MockDomainEvent mockDomainEvent)
+		{
+			LastEventOccurred = mockDomainEvent.OccurredOn;
+		}
+	}
+}
