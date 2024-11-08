0cea3770ccdfe3df3a3a98ee4c1bc3c2ff89b4e8
Sébastien Eveno <sebastien.louis.eveno@gmail.com>
2024-11-08 21:55:38 +0100

    Add PlanetsController

---
+++ StellarEmpires/WebApi/v1/Dtos/CreatePlanetDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StellarEmpires.WebApi.v1.Dtos;
+
+public sealed record CreatePlanetDto
+{
+	public Guid Id { get; init; } = Guid.NewGuid();
+	public string Name { get; init; } = string.Empty;
+	public bool IsColonized { get; init; } = false;
+	public Guid? ColonizedBy { get; init; } = null;
+	public DateTime? ColonizedAt { get; init; } = null;
+}
