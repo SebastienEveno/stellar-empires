ff324a431dec339b95cf9028dfa86c950c24a3d0
Sébastien Eveno <sebastien.louis.eveno@gmail.com>
2024-11-08 18:29:02 +0100

    Store/retrieve planets initial state to/from a JSON file

---
+++ StellarEmpires/Infrastructure/PlanetStore/IPlanetStore.cs
@@ -0,0 +1,10 @@
+﻿using StellarEmpires.Domain.Models;
+
+namespace StellarEmpires.Infrastructure.PlanetStore;
+
+public interface IPlanetStore
+{
+	Task SavePlanetAsync(Planet planet);
+	Task<List<Planet>> GetPlanetsAsync();
+	Task<Planet?> GetPlanetByIdAsync(Guid planetId);
+}
