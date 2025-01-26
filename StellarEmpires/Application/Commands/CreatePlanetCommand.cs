f7f8c732b2f55973dd83c8895147f4a26e7771d5
Sébastien Eveno <sebastien.louis.eveno@gmail.com>
2025-01-26 20:19:52 +0100

    Add CreatePlanetCommandHandler

---
+++ StellarEmpires/Application/Commands/CreatePlanetCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace StellarEmpires.Application.Commands;
+
+public record CreatePlanetCommand
+{
+	public required Guid Id { get; init; }
+	public required string Name { get; init; }
+	public required bool IsColonized { get; init; }
+	public Guid? ColonizedBy { get; init; }
+	public DateTime? ColonizedAt { get; init; }
+}
